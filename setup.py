f165648faf21e9e23e36edb802250c35c6d928fe
Angelin01 <angeloborsoiross@hotmail.com>
2019-06-16 16:03:19 -0300

    Basic project structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='cwb-bus',
+	version='0.0.1',
+	packages=[''],
+	package_dir={'': 'cwb-bus'},
+	url='https://github.com/killertux/cwb-bus',
+	license='MIT',
+	author='Angelin01, killertux, Kabbah',
+	author_email='',
+	description='Package aimed at facilitating the use of the open data from Curitiba\'s public transport system'
+)
