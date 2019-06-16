2e3c6f61ebe44de73507b0efdeed33fec9d98b7c
Angelin01 <angeloborsoiross@hotmail.com>
2019-06-16 17:23:28 -0300

    Downloader uses aiohttp and is not a class

--- cwb-bus/downloader.py
+++ cwb-bus/downloader.py
@@ -1,10 +1,6 @@
+import asyncio
+import aiohttp 
 from datetime import datetime
-from .filetype import FileType
+from filetype import FileType
 
-
-class Downloader:
-	
-	def __init__(self, date_start: datetime = datetime(2017,1,18), date_end: datetime = datetime.now(), file_type: FileType = FileType.ALL):
-		self.file_type = file_type
-		self.date_end = date_end
-		self.date_start = date_start
+_session = aiohttp.ClientSession()
