d3cc2c8da830790c798cd87ba1a9901414904209
Angelin01 <angeloborsoiross@hotmail.com>
2019-06-26 22:23:35 -0300

    Basic decompress function wrapper
    Needs to treat .tar.gz files still

--- cwb-bus/downloader.py
+++ cwb-bus/downloader.py
@@ -1,16 +1,20 @@
 import asyncio
-import aiohttp 
+import aiohttp
+from lzma import decompress as decompress_xz
+from zlib import decompress as decompress_gzip
 from datetime import date
 from .filetype import FileType
 
 
-_session = aiohttp.ClientSession()
+#_session = aiohttp.ClientSession()
 _download_folder = "./cache/downloads/"
 
 
-async def _download_file(data_date: date, session: aiohttp.ClientSession, data_type: FileType = None,
-                         base_url: str = "http://dadosabertos.c3sl.ufpr.br/curitibaurbs/") -> bytes:
-	"""Internal function that downloads the data.
+async def _download_file(data_date: date, data_type: FileType, session: aiohttp.ClientSession,
+                         base_url: str = "http://dadosabertos.c3sl.ufpr.br/curitibaurbs/",
+                         timeout: int = 10) -> bytes:
+	"""
+	Internal function that downloads the data.
 
 	This function will try to grab the data from the server using asynchronous requests.
 	Will try to grab from the `base_url` plus the types specified and append an extension.
@@ -19,20 +23,27 @@ async def _download_file(data_date: date, session: aiohttp.ClientSession, data_t
 
 	:param data_date: A :class:`datetime.date` instance with the specific date for the data that wants to be gathered
 	:param session: A :class:`aiohttp.ClientSession` session for downloading. Should probably only instance 1 for an entire operation
-	:param data_type: What type of data to download. If set to `None`, will download all types. See :class:`FileType` for available types
+	:param data_type: What type of data to download. See :class:`FileType` for available types
 	:param base_url: The base url for downloading data. Shouldn't need to be changed ever
+	:param timeout: Timeout value for the download until giving up
 	:return: a `bytes` compressed object, which needs to be decompressed. See :function:`_uncompress_file`
 	"""
 	pass
 
 
-async def _uncompress_file(file: bytes) -> bytes:
+def _decompress_file(file: bytes, is_xz: bool = True) -> bytes:
 	"""
+	Simple internal function as a small wrapper for decompressing files
 
-	:param file:
-	:return:
+	:param file: The bytes like object for the file received from the downloader
+	:param is_xz: Whether the file is a xz type file (if True) or a tar.gz file (if False)
+	:return: The compressed bytes object
 	"""
-	pass
+	if is_xz:
+		return decompress_xz(file)
+	# TODO Treat tar.gz files because they are going to return a tar file
+	# else:
+	# 	yield decompress_gzip(file)
 
 
 async def get_data(data_date: date, data_type: FileType = None, from_folder: str = None):
